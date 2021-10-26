aae4774dd06b4b696638f5da072b98288f9db808
Bowei Xu <boweixu929@gmail.com>
2021-10-25 23:20:36 -0400

    fixed a minor bug in async part

--- client/main.js
+++ client/main.js
@@ -1,41 +1,69 @@
 "use strict"
 let satellitePosAndVelo = {};
+import * as THREE from './js/three.module.js';
+import { OrbitControls } from './js/controls/OrbitControls.js';
+import { ConvexGeometry } from './js/geometries/ConvexGeometry.js';
+import * as BufferGeometryUtils from './js/utils/BufferGeometryUtils.js';
+import { GUI } from './js/libs/dat.gui.module.js';
+import { LightProbeGenerator } from './js/lights/LightProbeGenerator.js';
+
 window.addEventListener("load", () => {
     init();
     const socket = io();
-    socket.on('connect', socket=>{
+    socket.on('connect', socket => {
     });
-    socket.on('init', msg=>[
+    socket.on('init', msg => {
+        console.log(`get data: ${Object.keys(msg)}`);
         console.log(msg)
-    ])
-    socket.on('updateData', msg=>{
         satellitePosAndVelo = msg;
-        console.log(satellitePosAndVelo);
-        console.log('updated!')
+    })
+    socket.on('updateData', msg => {
+        satellitePosAndVelo = msg;
+        console.log(`get data: ${Object.keys(msg)}`)
     });
 });
 function init() {
+    const earthOribitLength = 5;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    const axesHelper = new THREE.AxesHelper(2);
-    axesHelper.setColors(0x555555, 0x555555, 0x555555);
+    const axesHelper = new THREE.AxesHelper(20);
+    axesHelper.setColors(0x999999, 0x999999, 0x999999);
     scene.add(axesHelper);
-    camera.position.z = 5;
-    camera.position.x = .5;
-    camera.position.y = .5;
+    camera.position.z = 20;
+    camera.position.x = 1;
+    camera.position.y = 1;
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 10;
+    controls.maxDistance = 100;
+    controls.maxPolarAngle = Math.PI;
+    const sphereGeo = new THREE.SphereGeometry(earthOribitLength, 100, 100);
+    const material = new THREE.LineDashedMaterial({
+        color: 0xffffff,
+        linewidth: .1,
+        scale: 1,
+        dashSize: 3,
+        gapSize: 1,
+    });
+    const sphere = new THREE.Mesh(sphereGeo, material);
+    scene.add(sphere);
     (function animate() {
         requestAnimationFrame(animate); //loop call
         renderer.render(scene, camera);
         // controls.update();
     })();
+    (function drawSatellites(satelliteObject) {
+        for (let satellite in satelliteObject) {
+            let tempSatellitePosition = satellite[position];
 
-
-
-
+        }
+    })(satellitePosAndVelo);
 
 
 
@@ -44,14 +72,14 @@ function init() {
 
 
     //utils
-    //window resize callback
+    //window resize
     function updateView() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
-
 }
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -3,14 +3,9 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const app = express();
-const util = require("util");
-const dotenv = require("dotenv");
 const veloCalc = require('./utils/velocityCalc');
-const starLinkData = require('./utils/rawPV.json');
-const roundDown = require('./utils/roundDown');
-const data = require('./dataToUse.json');
+const starLinkData = require('./rawPV.json');
 const fs = require('fs');
-dotenv.config();
 const PORT = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, "../client")));
 app.get("/", (req, res) => {
@@ -22,15 +17,11 @@ httpServer.listen(PORT, () => {
 });
 const io = require("socket.io")(httpServer);
 io.on("connection", socket => {
-    // console.log(data);
-    socket.emit('init', data);
-    setInterval(async () => {
-        await veloCalc();
-        await roundDown(starLinkData);
-        let dataToSend = await fs.readFileSync('./dataToUse.json', 'utf-8', (err) => {
-            if (err) console.log(err);
-        });
-        console.log(dataToSend);
-        socket.emit('updateData', dataToSend);
-    }, 20000);
+    socket.emit('init', starLinkData);
+    setInterval(() => {
+        veloCalc();
+        console.log('calc done')
+        console.log(starLinkData);
+        socket.emit('updateData', starLinkData);
+    }, 3e5);
 });
